839729ca84f76f4551ee1f10d7820658c9ef02cf
FunkyMR <j.czyz@ministerstworeklamy.pl>
2025-05-19 12:20:35 +0200

    Selenium fix 0.1

--- cookiescanner/views.py
+++ cookiescanner/views.py
@@ -42,9 +42,12 @@ class CookieScanView(APIView):
 
     def scan_cookies(self, domain):
         options = Options()
-        options.headless = True
-        options.add_argument("--disable-gpu")
+        # options.headless = True
+        # options.add_argument("--disable-gpu")
+        # options.add_argument("--no-sandbox")
+        options.add_argument("--headless")
         options.add_argument("--no-sandbox")
+        options.add_argument("--disable-dev-shm-usage")
 
         try:
             driver = webdriver.Chrome(options=options)
